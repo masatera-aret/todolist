a72593c8cb467206cf5a43b8ac3f3fe8340f415b
Masato Terashima <cye_ma_kun245@yahoo.co.jp>
2020-09-11 10:16:28 +0900

    remove firebaseConfig on Git

--- src/store/store.js
+++ src/store/store.js
@@ -4,20 +4,11 @@ import firebase from 'firebase/app'
 import 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+import firebaseConfig from './firebaseConfig'
 // import router from '../router/router'
 
 Vue.use(Vuex)
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA_11LX3CsIDPjHkSWNCurI6CIj4SaewvA",
-  authDomain: "note-pad-fe946.firebaseapp.com",
-  databaseURL: "https://note-pad-fe946.firebaseio.com",
-  projectId: "note-pad-fe946",
-  storageBucket: "note-pad-fe946.appspot.com",
-  messagingSenderId: "167740246624",
-  appId: "1:167740246624:web:3385c7a4a6a4af2f8c1105",
-  measurementId: "G-RLCYKBXHFP"
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
